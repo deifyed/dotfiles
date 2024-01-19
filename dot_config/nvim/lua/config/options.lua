14c056b1c251c8afb7c126773b01c2061427edf7
Julius Pedersen <julius@tuuturu.io>
2024-01-19 11:13:55 +0100

    ✅ Add norwegian letter abbreviations to vim config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.mouse = ""
+vim.opt.colorcolumn = "120"
+
+vim.cmd("ab aa å")
+vim.cmd("ab AA Å")
+vim.cmd("ab ae æ")
+vim.cmd("ab AE Æ")
+vim.cmd("ab oe ø")
+vim.cmd("ab OE Ø")
