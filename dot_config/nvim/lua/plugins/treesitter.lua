49d608dd739eef64996e02b41da8c1ff20172c81
Julius Pedersen <julius@tuuturu.io>
2025-02-08 22:26:15 +0100

    🐛 Fix vimwiki-fix breaking treesitter highlighting

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  config = function()
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+
     vim.treesitter.language.register("markdown", "vimwiki")
   end,
 }
