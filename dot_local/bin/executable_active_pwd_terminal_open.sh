efcf7710902967603166f2bb9a45205c63a4ed08
Julius Pedersen <julius@tuuturu.io>
2025-03-21 20:49:16 +0100

    👌 Use ghostty as default terminal

--- dot_local/bin/executable_active_pwd_terminal_open.sh
+++ dot_local/bin/executable_active_pwd_terminal_open.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 #
 
-CURRENT_WORKING_DIRECTORY="$( swaymsg -t get_tree |
+CURRENT_WORKING_DIRECTORY="$(swaymsg -t get_tree |
   jq '.. | (.nodes? // empty)[] | select(.focused == true).pid? // empty' |
   xargs pstree -p | grep "\<tmux\>\|\<fish\>\|\<bash\>\|\<zsh\>\|\<sh\>" |
-  grep -o '[0-9]*' | xargs pwdx 2> /dev/null | cut -f2- -d' ' |
-  sort | tail -n 1 | tr -d '\n' )"
+  grep -o '[0-9]*' | xargs pwdx 2>/dev/null | cut -f2- -d' ' |
+  sort | tail -n 1 | tr -d '\n')"
+
+echo $CURRENT_WORKING_DIRECTORY
 
 if [ -d "$CURRENT_WORKING_DIRECTORY" ]; then
-	alacritty --working-directory "$CURRENT_WORKING_DIRECTORY" &
-	disown
+  ghostty --working-directory="$CURRENT_WORKING_DIRECTORY" --command=/usr/bin/zellij &
+  disown
 else
-	alacritty &
-	disown
+  ghostty &
+  disown
 fi
